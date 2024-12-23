f5d56c846c5554c6623cde8a7f4b8fb4934d49f1
Sankar491 <sivasankarrcks@gmail.com>
2024-12-23 12:34:50 +0530

    terraform

---
+++ main.tf
@@ -0,0 +1,9 @@
+module "vpc" {
+    source = "../terraform-vpc-module1"
+    environment = var.environment
+    project = var.project
+    cidr_block = var.cidr_block
+    public_subnets = var.public_subnets
+    private_subnets = var.private_subnets
+    database_subnets = var.database_subnets
+}
---
+++ variables.tf
@@ -0,0 +1,25 @@
+variable "environment" {
+  type = string
+  default = "dev"
+}
+
+variable "project" {
+    type = string
+    default = "expense"
+}
+
+variable "cidr_block" {
+    default = "10.0.0.0/16"
+}
+
+variable "public_subnets" {
+    default = ["10.0.1.0/24", "10.0.2.0/24"]  
+}
+
+variable "private_subnets" {
+    default = ["10.0.11.0/24", "10.0.12.0/24"]  
+}
+
+variable "database_subnets" {
+    default = ["10.0.21.0/24", "10.0.22.0/24"]  
+}
